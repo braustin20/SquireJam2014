b38449b6c1a41ac8d26a305cd9ec6f4e4cf7766a
Shanan Dalton <shanan_dalton@yahoo.com>
2014-04-26 14:13:11 -0400

    Changes to Boulder Cam
    No longer rotates strangely

--- SquireJam/Assets/Standard Assets/Scripts/Camera Scripts/SmoothFollow.js
+++ SquireJam/Assets/Standard Assets/Scripts/Camera Scripts/SmoothFollow.js
@@ -29,7 +29,7 @@ function LateUpdate () {
 		return;
 	
 	// Calculate the current rotation angles
-	var wantedRotationAngle = target.eulerAngles.y;
+	var wantedRotationAngle = 0;
 	var wantedHeight = target.position.y + height;
 		
 	var currentRotationAngle = transform.eulerAngles.y;

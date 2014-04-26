ae89d30fb8c262a89d2996ff4a37f95b4f2b1f82
seanybaby2 <sepinnock@gmail.com>
2014-04-26 19:23:00 -0400

    Newest Updates to  Level Castle and Moving Buses

---
+++ SquireJam/Assets/Scripts/TruckBussTravel.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class TruckBussTravel : MonoBehaviour {
+
+	public Transform waypoint1;
+	public Transform waypoint2;
+	
+	private Vector3 dir;
+	
+	public float speed = 10;
+
+
+	// Use this for initialization
+	void Start () {
+		dir = waypoint1.position - transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.Translate(dir * (speed/50) * Time.deltaTime, Space.World);
+	
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if(other.gameObject.name == "WayPoint1"){
+			Debug.Log ("working");
+			dir = waypoint2.position - transform.position;
+		}
+		if(other.gameObject.name == "WayPoint2"){
+			Debug.Log ("working");
+			dir = waypoint1.position - transform.position;
+		}
+	}
+}

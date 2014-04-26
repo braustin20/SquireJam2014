040b30ab298092514af63032c7ee852f329864b2
Shanan Dalton <shanan_dalton@yahoo.com>
2014-04-26 14:03:44 -0400

    Terrain Generation
    Not completed yet

--- SquireJam/Assets/Scripts/TerrainGenerator.cs
+++ SquireJam/Assets/Scripts/TerrainGenerator.cs
@@ -3,18 +3,28 @@ using System.Collections;
 
 public class TerrainGenerator : MonoBehaviour {
 
-	public GameObject StartTerrain;
-	public GameObject[] TerrainPieces;
 
-	void Start () {
+	public Terrain StartTerrain;
+	public Terrain[] TerrainPieces;
 
-		Object start = Instantiate(StartTerrain);
-	
+	private Terrain start;
+	private Vector3 terrainSize;
+	private Vector3 terrainLocation;
+
+	void Start(){
+
+		terrainSize = StartTerrain.terrainData.size;
+		terrainLocation = new Vector3(0, 0, 0);
+		start = Instantiate(StartTerrain, terrainLocation, Quaternion.identity) as Terrain;
+
+		Instantiate(TerrainPieces[0], new Vector3(0, 0, terrainSize.z), Quaternion.identity);
+
+
 	}
 	
 	void Update () {
 
-
+		print(start.terrainData.size);
 
 	}
 }

debce99eeb7ab825ae5b22e317dbc4b55aa05448
Shanan Dalton <shanan_dalton@yahoo.com>
2014-04-26 12:49:30 -0400

    Terrain Generation
    Basic structure for it
    Still needs work

---
+++ SquireJam/Assets/Scripts/TerrainGenerator.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TerrainGenerator : MonoBehaviour {
+
+	public GameObject StartTerrain;
+	public GameObject[] TerrainPieces;
+
+	void Start () {
+
+		Object start = Instantiate(StartTerrain);
+	
+	}
+	
+	void Update () {
+
+
+
+	}
+}

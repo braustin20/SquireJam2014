15dab77926df2e9801ff6328c47d82780b6ec96b
Tarwine <tarwine0406@gmail.com>
2014-04-26 18:15:34 -0400

    Roller control changes

--- SquireJam/Assets/Scripts/RollerControls.cs
+++ SquireJam/Assets/Scripts/RollerControls.cs
@@ -9,6 +9,12 @@ public class RollerControls : MonoBehaviour {
 	public float constantSpeed = 1.0f;
 	public float acceleration = 1.0f;
 
+	public float groundedDelay = 1.0f;
+	private float timer;
+
+	public bool grounded = false;
+	private bool groundedTimerActive = false;
+
 	// Use this for initialization
 	void Start () {
 		boulder = GameObject.Find("Boulder");
@@ -17,6 +23,9 @@ public class RollerControls : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		if(groundedTimerActive){
+
+		}
 
 		if(Input.GetKey(KeyCode.A)){
 			boulder.rigidbody.AddForce(new Vector3(-1.0f, 0, 0) * strafeSpeed); 
@@ -29,6 +38,16 @@ public class RollerControls : MonoBehaviour {
 		}
 		if(Input.GetKey(KeyCode.S)){
 			boulder.rigidbody.AddForce(new Vector3(0, 0, -1.0f) * acceleration); 
+		}
+	}
+	void OnCollisionExit(Collision other){
+		if(other.collider.gameObject.tag == "Terrain"){
+			groundedTriggerActive = false;
+		}
+	}
+	void OnCollisionEnter(Collision other){
+		if(other.collider.gameObject.tag == "Terrain"){
+			groundedTriggerActive = true;
 		}
 	}
 }

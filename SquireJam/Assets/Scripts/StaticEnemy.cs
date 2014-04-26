efcaddca6d0d43699f8fa3d38bb4212a7321af34
Tarwine <tarwine0406@gmail.com>
2014-04-26 17:21:55 -0400

    Static enemy changes

--- SquireJam/Assets/Scripts/StaticEnemy.cs
+++ SquireJam/Assets/Scripts/StaticEnemy.cs
@@ -5,7 +5,7 @@ public class StaticEnemy: MonoBehaviour {
 	private bool destroyed = false;
 	private float timer = 0.0f;
 
-	public float deathTime = 3.0f;
+	public float deathTime = 2.0f;
 
 	// Use this for initialization
 	void Start () {
@@ -14,17 +14,20 @@ public class StaticEnemy: MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		timer += Time.deltaTime;
 		if(destroyed){
+			timer += Time.deltaTime;
+
 			if(timer >= deathTime){
-				GameObject.Destroy(this);
+				Debug.Log(timer);
+				GameObject.Destroy(this.gameObject);
 			}
 		}
 	}
 	void OnTriggerEnter(Collider other)
 	{
 		if(other.gameObject.name == "Boulder"){
-			rigidbody.AddExplosionForce(3.0f, other.transform.position, 10.0f);
+			rigidbody.AddExplosionForce(300.0f, other.gameObject.transform.position, 100.0f);
+			Debug.Log ("hit object");
 			destroyed = true;
 		}
 	}

4c5d15a5dc8bb6e53959c116c1dc95a9fe6aa60b
TheBeardedOne <jeremy.boggs42@gmail.com>
2014-04-26 12:44:59 -0400

    Properties Scripts

---
+++ SquireJam/Assets/Scripts/propertyBehavior.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class propertyBehavior : MonoBehaviour {
+
+	public bool onFire;
+	public bool onWater;
+	public bool onIce;
+	public bool onSnow;
+	public bool onEarth;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		constantForce.force = new Vector3 (0, 0, 1);
+
+
+	}
+}

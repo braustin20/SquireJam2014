915be413a2bb6c7527a51facb8205fc1d97d9370
TheBeardedOne <jeremy.boggs42@gmail.com>
2014-04-26 13:43:26 -0400

    Script Changes
    beginning to resize boulder; My prototype room added;

--- SquireJam/Assets/Scripts/propertyBehavior.cs
+++ SquireJam/Assets/Scripts/propertyBehavior.cs
@@ -8,7 +8,7 @@ public class propertyBehavior : MonoBehaviour {
 	public bool onIce;
 	public bool onSnow;
 	public bool onEarth;
-
+	public float newScale;
 
 	// Use this for initialization
 	void Start () {
@@ -19,7 +19,7 @@ public class propertyBehavior : MonoBehaviour {
 	void Update () {
 
 		if (onFire == true) {
-
+			growBoulder();
 				}
 		if (onWater == true) {
 				
@@ -33,12 +33,14 @@ public class propertyBehavior : MonoBehaviour {
 		if (onEarth == true) {
 				
 				}
-		
+
 	}
 
 	//This function will run code that increases the size of the boulder
 	void growBoulder(){
+		newScale = Mathf.Lerp(1, 5, Time.time);
 
+		gameObject.transform.localScale = new Vector3(newScale, 1, newScale);
 	}
 
 	//This function will run code that decreases the size of the boulder
@@ -47,20 +49,20 @@ public class propertyBehavior : MonoBehaviour {
 	}
 
 	//Function where we will change the boolean variables for the most recent property encountered
-	void OnCollisionEnter(Collider otherObject){
-		if (otherObject.tag == "fire") {
+	void OnCollisionEnter(Collision otherObject){
+		if (otherObject.gameObject.tag == "fire") {
 
 				}
-		if (otherObject.tag == "water") {
+		if (otherObject.gameObject.tag == "water") {
 				
 				}
-		if (otherObject.tag == "ice") {
+		if (otherObject.gameObject.tag == "ice") {
 
 				}
-		if (otherObject.tag == "earth") {
+		if (otherObject.gameObject.tag == "earth") {
 				
 				}
-		if (otherObject.tag == "snow") {
+		if (otherObject.gameObject.tag == "snow") {
 				
 				}
 	}
